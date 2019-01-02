dbd9617df87c2f6b17a4a5bfee43038ff8751bec
Tiago Cordeiro <tiago@mulhergorila.com>
2019-01-01 22:44:07 -0200

    Refatora Typo

--- utils/termdraw.py
+++ utils/termdraw.py
@@ -90,8 +90,10 @@ class Marquee:
 
 
 class _Getch:
-    """Gets a single character from standard input.  Does not echo to the
-screen."""
+    """
+    Gets a single character from standard input.
+    Does not echo to the screen.
+    """
 
     def __init__(self):
         try:
